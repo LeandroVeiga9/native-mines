f6d6ab0ae91706d77ee4d339dddc4b0dc5ccdcea
LeandroVeiga9 <leandro.veiga.clear@gmail.com>
2022-07-27 09:34:03 -0300

    create components

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import params from './src/params';
+import Field from './src/components/Field';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.welcome}>iniciando o mines</Text>
+      <Text style={styles.instructions}>tamanho da grade: {params.getRowsAmount()} x {params.getColumsAmount()}</Text>
+      <Field />
+      <Field opened />
+      <Field opened nearMines={1} />
+      <Field opened nearMines={2} />
+      <Field opened nearMines={3} />
+      <Field opened nearMines={6} />
+      <Field mined />
+      <Field mined opened />
+      <Field mined opened exploded />
+      <Field flagged />
+      <Field flagged opened />
     </View>
   );
 }
